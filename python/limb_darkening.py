ffb6312832522624567708a4d60c461b81c11569
Iva Laginja <ilaginja@stsci.edu>
2018-04-26 19:10:22 +0200

    Fixed up some minor stuff in the LD3D code.

--- python/limb_darkening.py
+++ python/limb_darkening.py
@@ -32,15 +32,17 @@ def limb_fit_3D_choose(grating, widek, wsdata, M_H, Teff, logg, dirsen, direc):
     print('  ' + direc)
 
     # Select Metallicity
-    M_H_Grid = np.array([-3.0, -2.0, -1.0, 0.0])  # Grid values points
+    M_H_Grid = np.array([-3.0, -2.0, -1.0, 0.0])  # Grid values points with metallicity values available to us
     M_H_Grid_load = ['30', '20', '10', '00']  # Grid values points
-    optM = (abs(M_H - M_H_Grid)).argmin()
+    optM = (abs(M_H - M_H_Grid)).argmin()  # Check which available grid value our input is closest to user input (M_H).
 
     # Select Teff
-    Teff_Grid = np.array([4000, 4500, 5000, 5500, 5777, 6000, 6500, 7000])
-    optT = (abs(Teff - Teff_Grid)).argmin()
+    Teff_Grid = np.array(
+        [4000, 4500, 5000, 5500, 5777, 6000, 6500, 7000])  # Grid value points with Teff values available to us
+    optT = (abs(Teff - Teff_Grid)).argmin()  # Check which available grid value our input is closest to
 
-    # Select logg
+    # Select logg, depending on Teff. If several logg possibilities are given for one Teff, pick the one that is closest
+    # to user input (logg).
 
     if Teff_Grid[optT] == 4000:
         logg_Grid = np.array([1.5, 2.0, 2.5])
@@ -74,36 +76,39 @@ def limb_fit_3D_choose(grating, widek, wsdata, M_H, Teff, logg, dirsen, direc):
         logg_Grid = [4.5]
         optG = 0
 
-    # ==== Select Teff and Log g
-    mtxt = M_H_Grid_load[optM]
+    # ==== Select Teff and Log g. Mtxt, Ttxt and Gtxt are then put together as string to load correct files.
+    Mtxt = M_H_Grid_load[optM]
     Ttxt = "{:2.0f}".format(Teff_Grid[optT] / 100)
     if (Teff_Grid[optT] == 5777):
         Ttxt = "{:4.0f}".format(Teff_Grid[optT])
-
     Gtxt = "{:2.0f}".format(logg_Grid[optG] * 10)
-    file = 'mmu_t' + Ttxt + 'g' + Gtxt + 'm' + mtxt + 'v05.flx'
-    model = file
-    header = file
-    #  if (header eq 0) then goto,skipthis 
-    sav = readsav(os.path.join(direc, file))
-    ws = sav['mmd'].lam[0]  # wavelength
-    f = sav['mmd'].flx
+
+    #
+    file = 'mmu_t' + Ttxt + 'g' + Gtxt + 'm' + Mtxt + 'v05.flx'
+    model = file  # not used anymore
+    header = file  # not used anymore
+    #  if (header eq 0) then goto,skipthis
+
+    # Read data from IDL .sav file
+    sav = readsav(os.path.join(direc, file))  # readsav reads an IDL .sav file
+    ws = sav['mmd'].lam[0]  # read in wavelength
+    flux = sav['mmd'].flx  # read in flux
     Teff_model = Teff_Grid[optT]
     logg_model = logg_Grid[optG]
     MH_model = str(M_H_Grid[optM])
     print('  ' + header)
 
-    f0 = f[0]
-    f1 = f[1]
-    f2 = f[2]
-    f3 = f[3]
-    f4 = f[4]
-    f5 = f[5]
-    f6 = f[6]
-    f7 = f[7]
-    f8 = f[8]
-    f9 = f[9]
-    f10 = f[10]
+    f0 = flux[0]
+    f1 = flux[1]
+    f2 = flux[2]
+    f3 = flux[3]
+    f4 = flux[4]
+    f5 = flux[5]
+    f6 = flux[6]
+    f7 = flux[7]
+    f8 = flux[8]
+    f9 = flux[9]
+    f10 = flux[10]
 
     # Mu from grid
     # 0.00000    0.0100000    0.0500000     0.100000     0.200000     0.300000   0.500000     0.700000     0.800000     0.900000      1.00000
@@ -133,8 +138,7 @@ def limb_fit_3D_choose(grating, widek, wsdata, M_H, Teff, logg, dirsen, direc):
         wdel = 4.882
     # =============
 
-
-# FOR WFC3
+    # FOR WFC3
     if grating == 'G141':  # http://www.stsci.edu/hst/acs/analysis/reference_files/synphot_tables.html
         sav = readsav(os.path.join(dirsen, 'G141.WFC3.sensitivity.sav'))  # wssens, sensitivity
         wssens = sav['wssens']
@@ -149,8 +153,6 @@ def limb_fit_3D_choose(grating, widek, wsdata, M_H, Teff, logg, dirsen, direc):
     # =============
     # =============
 
-
-
     wsHST = wssens
     wsHST = np.concatenate((np.array([wsHST[0] - wdel - wdel, wsHST[0] - wdel]),
                             wsHST,
@@ -163,10 +165,8 @@ def limb_fit_3D_choose(grating, widek, wsdata, M_H, Teff, logg, dirsen, direc):
     inter_resp = interp1d(wsHST, respoutHST, bounds_error=False, fill_value=0)
     respout = inter_resp(ws)  # interpolate sensitivity curve onto model wavelength grid
 
-    wsdata = np.concatenate((np.array([wsdata[0] - wdel - wdel, wsdata[0] - wdel]),
-                             wsdata,
-                             np.array([wsdata[len(wsdata) - 1] + wdel,
-                                       wsdata[len(wsdata) - 1] + wdel + wdel])))
+    wsdata = np.concatenate((np.array([wsdata[0] - wdel - wdel, wsdata[0] - wdel]), wsdata,
+                             np.array([wsdata[len(wsdata) - 1] + wdel, wsdata[len(wsdata) - 1] + wdel + wdel])))
     respwavebin = wsdata / wsdata * 0.0
     widek = widek + 2  # need to add two indicies to compensate for padding with 2 zeros
     respwavebin[widek] = 1.0
@@ -180,7 +180,7 @@ def limb_fit_3D_choose(grating, widek, wsdata, M_H, Teff, logg, dirsen, direc):
     fcalc = np.array([f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10])
     phot1 = np.zeros(11)
 
-    for i in range(11):  # do begin & $; loop over spectra at diff angles
+    for i in range(11):  # Loop over spectra at diff angles
         fcal = fcalc[i, :]
         Tot = int_tabulated(ws, ws * respout * reswavebinout)
         phot1[i] = (int_tabulated(ws, ws * respout * reswavebinout * fcal, sort=True)) / Tot
@@ -196,19 +196,23 @@ def limb_fit_3D_choose(grating, widek, wsdata, M_H, Teff, logg, dirsen, direc):
     # start fitting models
     fitter = LevMarLSQFitter()
 
+    # Fit a four parameter non-linear limb darkening model and get fitted variables, c1, c2, c3, c4.
     corot_4_param = nonlinear_limb_darkening()
     corot_4_param = fitter(corot_4_param, x, y)
     c1, c2, c3, c4 = corot_4_param.parameters
 
+    # Fit a three parameter non-linear limb darkening model and get fitted variables, cp2, cp3, cp4 (cp1 = 0).
     corot_3_param = nonlinear_limb_darkening()
     corot_3_param.c0.fixed = True  # 3 param is just 4 param with c0 = 0.0
     corot_3_param = fitter(corot_3_param, x, y)
     cp1, cp2, cp3, cp4 = corot_3_param.parameters
 
+    # Fit a quadratic limb darkening model and get fitted parameters aLD and bLD.
     quadratic = quadratic_limb_darkening()
     quadratic = fitter(quadratic, x, y)
     aLD, bLD = quadratic.parameters
 
+    # Fit a linear limb darkening model and get fitted variable uLD.
     linear = nonlinear_limb_darkening()
     linear.c0.fixed = True
     linear.c2.fixed = True
@@ -247,32 +251,39 @@ def int_tabulated(X, F, sort=False):
 
     # Compute the integral using the 5-point Newton-Cotes formula.
     ii = (np.arange((len(z) - 1) / 4, dtype=int) + 1) * 4
+
     return np.sum(2.0 * h * (7.0 * (z[ii - 4] + z[ii]) + 32.0 * (z[ii - 3] + z[ii - 1]) + 12.0 * z[ii - 2]) / 45.0)
 
 
 @custom_model
 def nonlinear_limb_darkening(x, c0=0.0, c1=0.0, c2=0.0, c3=0.0):
-    model = (1. - (c0 * (1. - x ** (1. / 2)) + c1 * (1. - x ** (2. / 2)) + c2 * (1. - x ** (3. / 2)) + c3 * (
-            1. - x ** (4. / 2))))
+    """
+    Define non-linear limb darkening model with four parameters c0, c1, c2, c3.
+    """
+    model = (1. - (c0 * (1. - x ** (1. / 2)) + c1 * (1. - x ** (2. / 2)) + c2 * (1. - x ** (3. / 2)) + c3 *
+                   (1. - x ** (4. / 2))))
     return model
 
 
 @custom_model
 def quadratic_limb_darkening(x, aLD=0.0, bLD=0.0):
+    """
+    Define linear limb darkening model with parameters aLD and bLD.
+    """
     model = 1. - aLD * (1. - x) - bLD * (1. - x) ** (4. / 2.)
     return model
 
 
 if __name__ == '__main__':
 
-    dirsen = '/Users/ilaginja/Documents/Git/HST_Marginalization/Limb-darkening/'
-    direc = '/Users/ilaginja/Documents/Git/HST_Marginalization/Limb-darkening/3DGrid/'
+    dirsen = os.path.join('..', 'Limb-darkening')
+    direc = os.path.join('..', 'Limb-darkening', '3DGrid')
     wavelength = np.loadtxt('W17_wavelength_test_data.txt', skiprows=3)
 
     # These numbers represent specific points in the grid for now. This will be updated to automatic grid selection soon.
-    FeH = 2  # Fe/H = -0.25
-    Teff = 139  # logg = 4.2, Teff = 6550 K - logg is incorporated into the temperature selection for now.
-    logg = 4.2
+    FeH = -0.25
+    Teff = 6550
+    logg = 4.2    # choice of logg depends on Teff
     grating = 'G141'
     wsdata = wavelength
     widek = np.arange(len(wavelength))
