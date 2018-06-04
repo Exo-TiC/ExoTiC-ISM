67fd9cd1d1d5641c6bdf3d516ac2136f5e9015e6
Hannah Wakeford <stellarplanet@gmail.com>
2018-06-04 13:20:53 -0400

    Read in a/R* instead of MsMpR and convert to MsMpR internally not in the user input.

--- python/W6_main.py
+++ python/W6_main.py
@@ -3,32 +3,10 @@ import os
 from G141_lightcurve_circle import G141_lightcurve_circle
 
 if __name__ == '__main__':
-    """
-    This is a translation of the W17_lightcurve_test.pro
-    """
-    # don't need them in the user input part
-    # SET THE CONSTANTS
-    dtosec = 86400
-    big_G = np.float64(6.67259e-11)
-    Rjup = np.float64(7.15e7)
-    Rsun = np.float64(6.96e8)
-    Mjup = np.float64(1.9e27)
-    Msun = np.float64(1.99e30)
-    HST_second = 5781.6
-    HST_period = 0.06691666
-
+    
     mainDir = '..'
     outDir = os.path.join(mainDir, 'outputs')
     dataDir = os.path.join(mainDir, 'data')
-
-    dtosec = 86400
-    big_G = np.float64(6.67259e-11)
-    Rjup = np.float64(7.15e7)
-    Rsun = np.float64(6.96e8)
-    Mjup = np.float64(1.9e27)
-    Msun = np.float64(1.99e30)
-    HST_second = 5781.6
-    HST_period = 0.06691666
 
 
     # READ in the txt file for the lightcurve data
@@ -38,24 +16,20 @@ if __name__ == '__main__':
     # SET-UP the parameters for the subroutine
     # ---------------------
     # PLANET PARAMETERS
-    rl = np.float64(0.13169232)  # Rp/R* estimate
-    epoch = np.float64(57879.633617736)  # in MJD
-    inclin = np.float64(88.41)  # this is converted into radians in the subroutine
+    rl = 0.13169232  # Rp/R* estimate
+    epoch = 57879.633617736  # in MJD
+    inclin = 88.41  # this is converted into radians in the subroutine
     ecc = 0.0  # set to zero and not used when circular
     omega = 0.0  # set to zero and not used when circular
-    Per = np.float64(3.36100239)  # in days, converted to seconds in subroutine
-
-    persec = Per * dtosec
-    aor = np.float64(10.94)  # a/r* converted to system density for the subroutine
-    constant1 = (big_G * persec * persec / np.float32(4 * 3.1415927 * 3.1415927)) ** (1 / 3.)
-    MsMpR = (aor / (constant1)) ** 3
+    Per = 3.36100239  # in days, converted to seconds in subroutine
+    aors = 10.94  # a/r* converted to system density for the subroutine
 
     ld_model = '1D'   # Which limb darkening models to use, '1D' or '3D'
     FeH = -0.2
     Teff = 5375
     logg = 4.487
 
-    data_params = [rl, epoch, inclin, MsMpR, ecc, omega, Per, FeH, Teff, logg]
+    data_params = [rl, epoch, inclin, aors, ecc, omega, Per, FeH, Teff, logg]
     grat = 'G141'   # Which grating to use
     grid_selection = 'fit_time'
     run_name = 'wl_time_1d'
